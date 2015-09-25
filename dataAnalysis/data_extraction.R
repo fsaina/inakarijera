1c2fa7430ad60e8f351aa85be0537ae232688bdf
Bartol <freskura.bartol@gmail.com>
2015-09-26 00:20:49 +0200

    code for loading data; added resources folder with sample.csv

--- dataAnalysis/data_extraction.R
+++ dataAnalysis/data_extraction.R
@@ -1,3 +1,22 @@
 ##SCRIPT FOR CONVERTING DATA FROM SURVEY CSV TO R DATA STRUCTURES
 
+#LOCAL path for csv files
+pathCsv <- "/mnt/8C1A960B1A95F286/Bartol_work/INA_Career_LOGO/resources"
+
+#Types of attributes in columns
+columnTypes <- c("character","character","character","integer","integer","integer","integer",
+                 "integer", "character", "character", "character", "character", "character", "character"
+                 , "character", "character", "character", "character","character","character")
+
+#Column names
+columnNames <- c("ID.Hash","Sex","Age","Student.Status","Employer.Opinion","National.Identity",
+                 "Employee.Satisfaction","Future.Employee","Work.Field","Industry","Creativity","Ecology",
+                 "Science","Energetics","Croatia","Association.Career", "E.Mail", "Start.Date","End.Date",
+                 "Network.ID")
+
+#Load data
+rawData <- read.csv(paste(pathCsv,"/sample_survey.csv", sep = ''),header = TRUE, na.strings = "",
+                    colClasses = columnTypes, comment.char = "")
+#Set column names
+colnames(rawData) <- columnNames
 
